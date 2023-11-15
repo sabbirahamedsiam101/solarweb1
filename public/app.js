b148d9c04be0ac4ebcb56bc674aee17922500210
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2023-11-16 01:35:28 +0600

    add

---
+++ public/app.js
@@ -0,0 +1,8 @@
+let button =  document.getElementById("button");
+let menu =  document.getElementById("menu");
+
+button.addEventListener("click",()=>{
+   menu.classList.toggle("hidden")
+   menu.classList.toggle("flex")
+
+})
